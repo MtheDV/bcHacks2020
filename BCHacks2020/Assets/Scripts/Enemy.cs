cb3ae1f71c44cbeb38f557c95597f5eacac5052a
Rick Feng <rickfeng99@gmail.com>
2020-01-18 18:01:42 -0800

    AI

---
+++ BCHacks2020/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private bool facingRight = false;
+    private Rigidbody2D rb2d;
+
+
+    [SerializeField] private float left;
+    [SerializeField] private float right;
+    [SerializeField] private float movementSpeed = 10f;
+
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (facingRight == false)
+        {
+            if (transform.position.x > left)
+            {
+                rb2d.velocity = new Vector2(-movementSpeed, 0);
+            }
+            else
+            {
+                facingRight = true;
+            }
+        }
+        else
+        {
+            if (transform.position.x < right)
+            {
+                rb2d.velocity = new Vector2(movementSpeed, 0);
+            }
+            else
+            {
+                facingRight = false;
+            }
+        }
+    }
+}
